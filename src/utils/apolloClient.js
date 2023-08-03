82d7806fe0a7ea5f02071f1fad41050bd977b77e
Minh Le <qminh279@gmail.com>
2023-08-02 23:53:48 -0400

    set up storage

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,5 +1,7 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import Constants from 'expo-constants';
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import Constants from "expo-constants";
+
+import { setContext } from "@apollo/client/link/context";
 
 const { apolloUri } = Constants.manifest.extra;
 
@@ -7,9 +9,25 @@ const httpLink = createHttpLink({
   uri: apolloUri,
 });
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : "",
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers,
+      };
+    }
+  });
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
