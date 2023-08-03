6fcfa081c0388c4e74076f63fa64578de8c6a1c2
Minh Le <qminh279@gmail.com>
2023-08-03 13:17:59 -0400

    sign out

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -4,6 +4,11 @@ import Constants from "expo-constants";
 import theme from "../../theme";
 import AppBarTab from "./AppBarTab";
 
+import { useQuery, useApolloClient } from "@apollo/client";
+import { GET_CURRENT_USER } from "../../graphql/queries";
+import useAuthStorage from "../../hooks/useAuthStorage";
+import { useNavigate } from "react-router-native";
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
@@ -15,11 +20,28 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  const { data } = useQuery(GET_CURRENT_USER);
+  const currentUser = data?.me;
+
+  const onSignOut = async () => {
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+    navigate('/');
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollView} horizontal>
         <AppBarTab to="/">Repositories</AppBarTab>
-        <AppBarTab to="/sign-in">Sign In</AppBarTab>
+        {currentUser ? (
+          <AppBarTab onPress={onSignOut}>Sign out</AppBarTab>
+        ) : (
+          <AppBarTab to="/sign-in">Sign In</AppBarTab>
+        )}
       </ScrollView>
     </View>
   );
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-import { REPOSITORY_BASE_FIELDS } from './fragments';
+import { REPOSITORY_BASE_FIELDS, USER_BASE_FIELDS } from './fragments';
 
 export const GET_REPOSITORIES = gql`
   query {
@@ -15,4 +15,14 @@ export const GET_REPOSITORIES = gql`
     }
   }
   ${REPOSITORY_BASE_FIELDS}
+`;
+
+export const GET_CURRENT_USER = gql`
+  query {
+    me {
+      ...userBaseFields
+    }
+  }
+
+  ${USER_BASE_FIELDS}
 `;
